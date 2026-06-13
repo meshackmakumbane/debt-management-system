8ec13ee2d8d9c1c9efd4542b483557afd2188385
Meshack Makumbane <meshackmakumbane@gmail.com>
2026-06-13 16:48:06 +0200

    Changes on cookies

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,8 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.set('trust proxy', 1);
+
 /* ---------------- MIDDLEWARE ---------------- */
 app.use(
   cors({
